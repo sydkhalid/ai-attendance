ed7108d98f39cd3f4f620d0c99e7c5259ffbbd3d
syed khalid ahamed <sydkhalid7@gmail.com>
2025-12-04 18:31:39 +0530

    Updateded Main Route Middleware To Server

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        web: __DIR__.'/../routes/web.php',   // ADD THIS LINE
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -17,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
