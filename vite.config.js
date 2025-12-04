0ef3241f12551cbad79a30e561b13f5c1236d469
syed khalid ahamed <sydkhalid7@gmail.com>
2025-12-04 05:51:29 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+
     build: {
-        manifest: true,
         outDir: 'public/build',
+        assetsDir: 'assets',
+        manifest: true,
         emptyOutDir: true,
         rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: {
+                app: 'resources/js/app.js',
+            },
         },
-    }
+    },
 });
